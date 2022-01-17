4ff61539383f111b12c4f5b927da4d151f9dc6d4
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-18 06:46:12 +0700

    a: Restaurant info setup part 2

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export const RestaurantInfo = () => {
-  return <Text>RestaurantInfo</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name= "Some Restaurants",
+    icon,
+    photos,
+    address,
+    openingHours,
+    rating,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
 };

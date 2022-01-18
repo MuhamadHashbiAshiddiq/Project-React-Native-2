4f0205b425cf43ad4501e0f7752c2c1d63c8da1f
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-18 12:24:33 +0700

    a: Setting up our theme

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -13,9 +13,11 @@ const SafeArea = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${StatusBar.currentHeight}px;
 `;
+
 const SearchContainer = styled(View)`
   padding: 16px;
 `;
+
 const RestaurantListContainer = styled(View)`
   flex: 1;
   padding: 16px;

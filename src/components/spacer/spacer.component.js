509ee7dcb61fb6a1f97180e232f57a31b9782ec6
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-19 09:33:58 +0700

    a: Optimizing the spacer component

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,46 +1,31 @@
-import React from "react";
 import { View } from "react-native";
 import styled from "styled-components";
 
-const TopSmall = styled(View)`
-  margin-top: ${(props) => props.theme.space[1]};
-`;
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3
+}
 
-const TopMedium = styled(View)`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
+const positionVariant = {
+  top: 'marginTop',
+  left: 'marginLeft',
+  right: 'marginRight',
+  bottom: 'marginBottom',
+}
 
-const TopLarge = styled(View)`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
+const getVariant = (placePosition, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[placePosition];
+  const value = theme.space[sizeIndex];
+  return `${property}:${value}`
+}
 
-const LeftSmall = styled(View)`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
+export const Spacer = styled(View)`
+${({placePosition, size, theme}) => getVariant(placePosition, size, theme)}
+`
 
-const LeftMedium = styled(View)`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled(View)`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
-export const Spacer = ({ variant }) => {
-  if (variant === "top.medium") {
-    return <TopMedium />;
-  }
-  if (variant === "top.Large") {
-    return <TopLarge />;
-  }
-  if (variant === "left.small") {
-    return <LeftSmall />;
-  }
-  if (variant === "left.medium") {
-    return <LeftMedium />;
-  }
-  if (variant === "left.large") {
-    return <LeftLarge />;
-  }
-  return <TopSmall />;
-};
+Spacer.defaultProps = {
+  placePosition: "top",
+  size: "small",
+}
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -91,15 +91,17 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <Spacer variant="left.large" />
-            {isOpenNow && (
-              <SvgXml xml={open} width={20} height={20} />
-            )}
-            <Spacer variant="left.large" />
-            <Image
-              style={{ width: 15, height: 15 }}
-              source={{ uri: icon }}
-            />
+            <Spacer placePosition="left" size="large">
+              {isOpenNow && (
+                <SvgXml xml={open} width={20} height={20} />
+              )}
+            </Spacer>
+            <Spacer placePosition="left" size="large">
+              <Image
+                style={{ width: 15, height: 15 }}
+                source={{ uri: icon }}
+              />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>

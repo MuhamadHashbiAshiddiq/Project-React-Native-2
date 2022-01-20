1588137b206644e6e3750e0d1b121fb782fb991c
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-20 12:22:17 +0700

    a: Solution activity indicator

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,6 +2,10 @@ import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components";
 import { View, FlatList } from "react-native";
+import {
+  ActivityIndicator,
+  Colors,
+} from "react-native-paper";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -18,12 +22,31 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(
     RestaurantsContext
   );
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading
+            size={50}
+            animating={true}
+            color={Colors.blue300}
+          />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>

99b5cfb2e66df8e02717c84c07818a88a304578d
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-20 16:49:04 +0700

    a: Searching for restaurants

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,5 +1,6 @@
 import React, {
   useState,
+  useContext,
   createContext,
   useEffect,
   useMemo,
@@ -9,6 +10,7 @@ import {
   restaurantsRequest,
   restaurantsTransform,
 } from "./restaurants.service";
+import { LocationContext } from "../location/location.context";
 
 export const RestaurantsContext = createContext();
 
@@ -18,11 +20,13 @@ export const RestaurantsContextProvider = ({
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { location } = useContext(LocationContext);
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
+    setRestaurants([]);
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(loc)
         .then(restaurantsTransform)
         .then((results) => {
           setIsLoading(false);
@@ -36,8 +40,11 @@ export const RestaurantsContextProvider = ({
   };
 
   useEffect(() => {
-    retrieveRestaurants();
-  }, []);
+    if (location) {
+      const locationString = `${location.lat},${location.lng}`;
+      retrieveRestaurants(locationString);
+    }
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider

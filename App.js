271c7bf323f07b939f95aee4dda431f131005cde
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-20 09:40:54 +0700

    a: Fake an API Request

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import {
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 
 const Tab = createBottomTabNavigator();
 

b7b6cf6a9f1d7deba62374358d1449c30e63007a
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-20 17:38:46 +0700

    a: Anti-pattern

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -16,11 +16,14 @@ export const LocationContextProvider = ({ children }) => {
   const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
-    if (!searchKeyword.length) {
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
       // don't do anything
       return;
     }
-    locationRequest(searchKeyword.toLowerCase())
+    locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
@@ -31,7 +34,7 @@ export const LocationContextProvider = ({ children }) => {
         setIsLoading(false);
         setError(err);
       });
-  };
+  }, [keyword]);
 
   return (
     <LocationContext.Provider

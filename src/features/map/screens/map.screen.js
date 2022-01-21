69d07f7fd6091ff14cf42b23975d00bab5c6c965
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-21 15:50:52 +0700

    a: Map screen and search

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -4,7 +4,18 @@ import React, {
   useContext,
 } from "react";
 import MapView from "react-native-maps";
+import styled from "styled-components";
+
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 
 export const MapScreen = () => (
-  <MapView style={{ height: "100%" }} />
+  <>
+    <Search />
+    <Map />
+  </>
 );
